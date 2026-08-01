7be56d0867453ec57e61b9a259576e52e16d176d
余锶埼 <177228770@qq.com>
2026-08-01 15:33:58 +0800

    update

---
+++ 模板/template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll LNF = 0x3f3f3f3f3f3f3f3fLL;
+
+void solve() {
+    // TODO
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;            // 多组样例；单组时删掉这行
+    while (T--) solve();
+
+    return 0;
+}
